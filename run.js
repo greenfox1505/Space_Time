168d81b7fedb8701a0b2db9c8ffe8eaf291f6811
Ryan Leverenz <Me@RyanLeverenz.net>
2017-03-01 07:56:16 +0000

    Holyshit it kinda works!

--- run.js
+++ run.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 
 var express = require('express')
+var bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
+
 var app = express()
 
 var topics = require("./topics.js")();
 
 var settings = require("./settings.js");
 
+var LastAction = 0;
 
 //static/index.html
 app.use('/', express.static('static'))
@@ -20,6 +24,25 @@ app.get('/API/GetTopicList',function(req,res){
 })
 app.get('/API/GetTopicData/:UUID',function(req,res){
 	res.send(topics.getTopicData(req.params.UUID));
+})
+
+app.put('/API/PutComment/:UUID', jsonParser ,function(req,res){
+	console.log("comment put request!", req.body);
+	
+	var comment = {
+		owner:req.body.name,
+		time:Date.now(),
+		text:req.body.comment
+		};
+	
+	topics.putComment(req.params.UUID,comment)
+	
+	res.send("comment recived" + req.body);
+	LastAction = comment.time;
+})
+
+app.get('/API/LastAction',function(req,res){
+	res.send({a:LastAction});
 })
 
 app.listen(settings.port, function () {
